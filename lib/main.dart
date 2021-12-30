71badca900e838ad7ec34cec35e6d210f2a9f5a4
FlutLab.io <>
2021-12-30 08:16:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/TestApi',
+      initialRoute: '/Welcome',
       routes: {
         '/Welcome': (context) => WelcomePage(),
         '/Button': (context) => ButtonComponent(),
