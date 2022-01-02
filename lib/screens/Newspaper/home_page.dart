1010a99e989c9b9dd7737a53a5785b653e68a972
FlutLab.io <>
2022-01-02 19:37:40 +0000

    Autosave

--- lib/screens/Newspaper/home_page.dart
+++ lib/screens/Newspaper/home_page.dart
@@ -22,6 +22,7 @@ class _Newspaper_homeState extends State<Newspaper_home> {
                   physics: NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   itemBuilder: (context, index) => Container(
+                    margin: EdgeInsets.all(10),
                     child: Row(
                       children: [
                         Image.asset("assets/person.png"),
