7bc2b49cf4241f7eaabc6f3a4933c149a2cb154a
FlutLab.io <>
2022-01-02 19:12:02 +0000

    Autosave

--- lib/screens/Newspaper/home_page.dart
+++ lib/screens/Newspaper/home_page.dart
@@ -9,44 +9,46 @@ class _Newspaper_homeState extends State<Newspaper_home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              color: Colors.pink,
-              height: 90,
-            ),
-            SizedBox(height: 40),
-            Container(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 200,
-                    child: ListView.builder(
-                      itemCount: 10,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) => Container(
-                        width: 100,
-                        height: 100,
-                        margin: EdgeInsets.all(7),
-                        child: Center(
-                          child: Column(
-                            children: [
-                              CircleAvatar(
-                                radius: 40,
-                                backgroundImage: AssetImage("assets/person.png"),
-                              ),
-                              Text("Card $index"),
-                            ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                color: Colors.pink,
+                height: 90,
+              ),
+              SizedBox(height: 40),
+              Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                        itemCount: 10,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => Container(
+                          width: 100,
+                          height: 100,
+                          margin: EdgeInsets.all(7),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage: AssetImage("assets/person.png"),
+                                ),
+                                Text("Card $index"),
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
