3dffee03eab19b6f0c4aac13d206af17531d6e8a
FlutLab.io <>
2022-01-02 19:11:23 +0000

    Autosave

--- lib/screens/Newspaper/home_page.dart
+++ lib/screens/Newspaper/home_page.dart
@@ -18,71 +18,32 @@ class _Newspaper_homeState extends State<Newspaper_home> {
             ),
             SizedBox(height: 40),
             Container(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("assets/person.png"),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 200,
+                    child: ListView.builder(
+                      itemCount: 10,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) => Container(
+                        width: 100,
+                        height: 100,
+                        margin: EdgeInsets.all(7),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 40,
+                                backgroundImage: AssetImage("assets/person.png"),
+                              ),
+                              Text("Card $index"),
+                            ],
+                          ),
                         ),
-                        Text("person"),
-                      ],
+                      ),
                     ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("assets/person.png"),
-                        ),
-                        Text("person"),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("assets/person.png"),
-                        ),
-                        Text("person"),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("assets/person.png"),
-                        ),
-                        Text("person"),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("assets/person.png"),
-                        ),
-                        Text("person"),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage: AssetImage("assets/person.png"),
-                        ),
-                        Text("person"),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
